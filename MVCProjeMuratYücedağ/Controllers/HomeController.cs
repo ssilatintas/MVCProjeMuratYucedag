55bbc439426a23c4c2b4bed0e1329132e89e33fa
lenovo <silatintas1@gmail.com>
2024-04-23 20:15:33 +0300

    3. Ders

--- MVCProjeMuratYücedağ/Controllers/HomeController.cs
+++ MVCProjeMuratYücedağ/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MVCProjeMuratYücedağ.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Senin Hakkında Sayfan.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Senin İletişim Sayfan.";
 
             return View();
         }
