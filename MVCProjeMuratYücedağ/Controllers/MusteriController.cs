d73ab59202cac1e391ea14d44b46ea82568574b0
lenovo <silatintas1@gmail.com>
2024-04-24 05:24:14 +0300

    15. Ders

---
+++ MVCProjeMuratYücedağ/Controllers/MusteriController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProjeMuratYücedağ.Models.Entity;
+
+namespace MVCProjeMuratYücedağ.Controllers
+{
+    public class MusteriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+    }
+}
