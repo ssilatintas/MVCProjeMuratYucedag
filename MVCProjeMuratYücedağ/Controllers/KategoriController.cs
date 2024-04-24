9fec1da86eef2e0fbf6cb65a571e822f8497c5c7
lenovo <silatintas1@gmail.com>
2024-04-24 04:55:50 +0300

    13. Ders

--- MVCProjeMuratYücedağ/Controllers/KategoriController.cs
+++ MVCProjeMuratYücedağ/Controllers/KategoriController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCProjeMuratYücedağ.Models.Entity;
 
 namespace MVCProjeMuratYücedağ.Controllers
 {
     public class KategoriController : Controller
     {
-        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
         }
     }
 }
